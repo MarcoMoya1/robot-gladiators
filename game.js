48ffdf4b2d29932e290645bb5dba890cbb67c646
MarcoMoya1 <98365526+MarcoMoya1@users.noreply.github.com>
2022-02-16 22:27:44 -0600

    Add files via upload

---
+++ game.js
@@ -0,0 +1,10 @@
+ var playerName =  window.prompt("What is your robot's name?");
+
+ console.log(playerName);
+ console.log(10+10);
+
+function fight() {
+    window.alert("The fight has begun!");
+}
+
+//fight();
