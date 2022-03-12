a0124b0f597ddbd2a3d29d8ecd64c685caf93a11
915marco <915marco@gmail.com>
2022-03-11 20:47:57 -0600

    first commit

---
+++ assets/js/game.js
@@ -0,0 +1,11 @@
+ var playerName =  window.prompt("What is your robot's name?");
+
+ console.log(playerName);
+ console.log(10+10);
+
+ //this creates a function named fight // 
+function fight() {
+    window.alert("The fight has begun!");
+}
+
+//fight();
